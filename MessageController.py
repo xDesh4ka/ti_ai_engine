081b42f8160f0a0b70008142fe3f3e7eadf0f4a3
v.aitbaev <v.aitbaev@bftcom.com>
2024-05-04 23:50:31 +0500

    second commit

--- MessageController.py
+++ MessageController.py
@@ -1,24 +1,35 @@
-import json
-import datetime
-
 from flask import Flask, request
+from flask_swagger_ui import get_swaggerui_blueprint
 
-from Answer import Answer, from_json
+from Answer import from_json
 from OutputMessage import OutputMessage
 from llamaEngine import AiEngine
 
 app = Flask(__name__)
 aiEngine = AiEngine()
 
+SWAGGER_URL = '/api'
+API_URL = '/'
+
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Ti Ai Engine'
+    }
+)
+
+app.register_blueprint(swagger_ui_blueprint)
 
 @app.route('/getReport', methods=['POST'])
 def get_report():
     data = request.get_json()
     message = OutputMessage(date=data['date'], text=data['text'])
-    ai_responce = aiEngine.ask_and_response(message.text).replace("\n", "")
-    answer = from_json(ai_responce).to_json()
+    ai_response = aiEngine.ask_and_response(message.text).replace("\n", "")
+    answer = from_json(ai_response).to_json()
     return answer
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8086, host="localhost", debug=True)
